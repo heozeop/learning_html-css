4ae88e374a52dd359a1e13e814853d35ff8a3844
Lee seung chan <dltmdcks702@gmail.com>
2021-03-18 16:42:02 +0900

    add flipping clock

--- 2021.3.21/clock.js
+++ 2021.3.21/clock.js
@@ -1,30 +1,63 @@
-function getTime() {
-  const time = new Date();
+class Flipper {
+  isFlipping = false;
+  duration = 550;
+  flipNode;
+  frontNode;
+  backNode;
 
-  const hour = time.getHours();
-  const minuate = time.getMinutes();
-  const seconds = time.getSeconds();
-
-  return { hour, minuate, seconds };
-}
-
-function formatSingle(num) {
-  if (num < 10) {
-    return `0${num}`;
+  constructor(node, currentTime, nextTime) {
+    this.flipNode = node;
+    this.frontNode = node.querySelector('.front');
+    this.backNode = node.querySelector('.back');
+    this.setFrontTime(currentTime);
+    this.setBackTime(nextTime);
   }
-  return num;
+
+  setFrontTime(time) {
+    this.frontNode.dataset.number = time;
+  }
+  setBackTime(time) {
+    this.backNode.dataset.number = time;
+  }
+
+  flipDown(currentTime, nextTime) {
+    if (this.isFlipping) {
+      return false;
+    }
+
+    this.isFlipping = true;
+    this.setFrontTime(currentTime);
+    this.setBackTime(nextTime);
+    this.flipNode.classList.add("running");
+    setTimeout(() => {
+      this.flipNode.classList.remove("running");
+      this.isFlipping = false;
+      this.setFrontTime(nextTime);
+    }, this.duration);
+  }
 }
 
-function setClock() {
-  const clock = document.querySelector('.clock');
-  const hourElm = clock.querySelector('#hour');
-  const minuateElm = clock.querySelector('#minuate');
-  const secondElm = clock.querySelector('#seconds');
-  const { hour, minuate, seconds } = getTime();
-  
-  hourElm.innerHTML = formatSingle(hour);
-  minuateElm.innerHTML = formatSingle(minuate);
-  secondElm.innerHTML = formatSingle(seconds);
+function getTimeFromDate(date) {
+  return date.toTimeString().slice(0, 8).split(":").join("");
 }
 
-setInterval(setClock, 1000);
+let flips = document.querySelectorAll('.flip');
+let now = new Date();
+let nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+let nextTimeStr = getTimeFromDate(now);
+let Flippers = Array.from(flips).map((flip, i) => {
+  return new Flipper(flip, nowTimeStr[i], nextTimeStr[i]);
+});
+
+setInterval(() => {
+  let now = new Date();
+  let nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+  let nextTimeStr = getTimeFromDate(now);
+  for (var i = 0; i < Flippers.length; ++i) {
+    if (nowTimeStr[i] === nextTimeStr[i]) {
+      continue;
+    }
+    Flippers[i].flipDown(nowTimeStr[i], nextTimeStr[i]);
+  }
+}, 1000);
+
