9b115c51d87d1c810ddafa0e53a5851fb9a0f91e
Lee seung chan <dltmdcks702@gmail.com>
2021-08-14 21:15:33 +0900

    set date range property

--- purejs/dateRange/dateRange.js
+++ purejs/dateRange/dateRange.js
@@ -2,6 +2,12 @@ class DateRange {
   static from(...args) {
     return new DateRange(...args);
   }
+
+  constructor(args) {
+    const today = new Date();
+    this.start = args?.start ?? today.getDate();
+    this.end = args?.end ?? new Date(today.getDate() + 1);
+  }
 }
 
 module.exports = DateRange;
