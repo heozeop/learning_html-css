7efbb0aba25751990dc611697a7f51894fd03c02
Lee seung chan <dltmdcks702@gmail.com>
2021-08-14 21:11:54 +0900

    add property check

--- purejs/dateRange/dateRange.test.js
+++ purejs/dateRange/dateRange.test.js
@@ -4,4 +4,13 @@ test('check is creatable', () => {
   const newDateRange = DateRange.from();
 
   expect(newDateRange).not.toBe(null);
+});
+
+test('created dateRange has default start date and end date properties', () => {
+  const newDateRange = DateRange.from();
+  const startDate = newDateRange.start;
+  const endDate = newDateRange.end;
+
+  expect(typeof startDate).not.toEqual("undefined");
+  expect(typeof endDate).not.toEqual("undefined");
 })
