520dd1f3b7f85a47e5216784a414e7f242e6f0eb
Lee seung chan <dltmdcks702@gmail.com>
2021-08-16 19:21:59 +0900

    add child function call test

--- purejs/bubles/bubble.test.ts
+++ purejs/bubles/bubble.test.ts
@@ -5,3 +5,27 @@ test('dummy dom element could be generated with from', ()=> {
 
   expect(typeof sampleElement).not.toBe('undefined');
 });
+
+test('bubble element has id and type which can be undefine ', ()=>{
+  const sampleElement = BubbleElement.from();
+  const sample2Element = BubbleElement.from('1');
+
+  expect(typeof sampleElement.id).toEqual('undefined');
+  expect(typeof sampleElement.type).toEqual('undefined');
+
+  expect(typeof sample2Element.id).toEqual('string');
+  expect(typeof sample2Element.type).toEqual('undefined');
+});
+
+describe('if one bubble element got child', ()=>{
+  const parentFunction = jest.fn((event: string) => {})
+  const childFunction = jest.fn((event: string) => {})
+  const parentElement = BubbleElement.from('1', 'parent', parentFunction);
+  const childElement = BubbleElement.from('2', 'child', childFunction);
+  parentElement.addChild(childElement);
+
+  test('trigger event on child event', () => {
+    childElement.onEvent('hello world');
+    expect(parentFunction).toHaveBeenCalledTimes(1);
+  })
+})
--- purejs/bubles/bubble.ts
+++ purejs/bubles/bubble.ts
@@ -1,13 +1,39 @@
-interface IBubbleElement {
-  id: string;
-  type: string;
-  onevent: (event: string) => void;
-  addchild: (child: IBubbleElement) => void;
-  removechild: (child: IBubbleElement) => void;
+interface IBubbleElement extends IBubble {
+  addChild: (child: IBubbleElement) => void;
+  removeChild: (child: IBubbleElement) => void;
 }
 
-export default class  BubbleElement {
-  static from(...args) {
+export interface IBubble {
+  id: string;
+  type: string;
+  onEvent: (event: string) => void;
+}
+
+export default class  BubbleElement implements IBubbleElement {
+  id: string;
+  type: string;
+  onEvent: (event: string) => void;
+
+  static from(...args: any[]) {
     return new BubbleElement(...args);
+  }
+
+  constructor(id: string, type: string, listener: (event: string) => void) {
+    this.id = id;
+    this.type = type;
+    this.onEvent = (event: string) => {
+      listener(event);
+    }
+  }
+
+  addChild(child: IBubbleElement) {
+    const duplicateChild = Object.assign({},child);
+    child.onEvent = (event: string) => {
+      duplicateChild.onEvent(event);
+      this.onEvent(event);
+    }
+  }
+  removeChild(child: IBubbleElement) {
+
   }
 }
